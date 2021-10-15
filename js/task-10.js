ee2f3ca05ee52ce5d1af99e6863077def7b3a2c1
Yuliia-Lysiuk <84859026+Yuliia-Lysiuk@users.noreply.github.com>
2021-10-16 01:18:04 +0300

    10 task done

--- js/task-10.js
+++ js/task-10.js
@@ -1,11 +1,32 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const ref = {
+ number: document.querySelector("input"),
+ btCreate: document.querySelector("[data-create]"),
+ btDestroy: document.querySelector("[data-destroy]"),
+ boxes: document.querySelector("#boxes"),
+}
 
-const number = document.querySelector("input");
+function createBox(amount) {
+  let boxSize = 30;
+  for (let i = 1; i <= amount; i += 1) {
+    const box = `<div style="width: ${boxSize}px; height: ${boxSize}px; background-color:${getRandomHexColor()};"></div>`
+    ref.boxes.insertAdjacentHTML("beforeend", box);
+    boxSize += 10;
+  }
+}
 
-const btCreate = document.querySelector("[data-create]");
+function destroyBox() {
+   ref.boxes.innerHTML = '';
+};
 
-const btDestroy = document.querySelector("[data-destroy]");
+ref.btCreate.addEventListener("click", () => {
+  createBox( ref.number.value);
+});
 
-btCreate.addEventListener("click", createBox)
+ref.btDestroy.addEventListener("click", () => {
+  destroyBox();
+   ref.number.value = "";
+});
+
